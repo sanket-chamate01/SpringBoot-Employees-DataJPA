a6ebba6f82261da0072f515183b2c567f3ac72a0
sanket-chamate01 <sanket.chamate2001@gmail.com>
2026-03-08 23:46:34 +0530

    taking user details from database

--- src/main/java/com/rest/employee/security/SecurityConfig.java
+++ src/main/java/com/rest/employee/security/SecurityConfig.java
@@ -5,20 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        // JdbcUserDetailsManager - tells spring security to use JDBC authentication with our data source
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+
+        return jdbcUserDetailsManager;
+    }
+
+    // hard-coded user details
+    /*
+    @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
         UserDetails john = User.builder()
                 .username("john")
-                .password("{noop}test123")
+                .password("{noop}test123") // bcrypt version - $2a$10$HdmZZUstFgqDTsW7RpXrBeVMFSaIztaQtAymLvjWkMNrcfhxhYsmK
                 .roles(RoleConstants.EMPLOYEE)
                 .build();
 
@@ -36,6 +48,7 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+     */
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
